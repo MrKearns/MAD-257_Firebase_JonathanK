c69c7ecc790fd450f4dc92c0e8d4d83d9b7b5f46
MrKearns <jkearns631@students.mchenry.edu>
2023-02-13 09:04:01 -0600

    Initial Commit

---
+++ MAD-257_Firebase_JonathanK/MAD_257_Firebase_JonathanKApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAD_257_Firebase_JonathanKApp.swift
+//  MAD-257_Firebase_JonathanK
+//
+//  Created by Jonathan Kearns on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MAD_257_Firebase_JonathanKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
